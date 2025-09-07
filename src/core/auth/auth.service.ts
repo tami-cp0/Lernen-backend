1b3d2b568dc32f506043cbf2af19e836b00ac45b
tami-cp0 <findtamilore>
2025-09-07 14:08:47 +0100

    fix message

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -340,7 +340,7 @@ export class AuthService {
 		const user = await this.dbService.db.query.users.findFirst({
 			where: eq(users.email, email),
 		});
-		if (!user) throw new BadRequestException('User not found');
+		if (!user) throw new BadRequestException('User account not found');
 
 		const resetToken = this.jwtService.sign(
 			{ sub: user.id, email: user.email, passwordReset: true },
