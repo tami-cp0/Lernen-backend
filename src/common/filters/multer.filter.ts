1325cba7075983ec1141f339fffe603085efbd94
tami-cp0 <findtamilore@gmail.com>
2026-01-10 10:39:24 +0100

    added cors to multer config

--- src/common/filters/multer.filter.ts
+++ src/common/filters/multer.filter.ts
@@ -1,24 +1,37 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpStatus } from '@nestjs/common';
+import {
+	ExceptionFilter,
+	Catch,
+	ArgumentsHost,
+	HttpStatus,
+} from '@nestjs/common';
 import { MulterError } from 'multer';
+import { Response } from 'express';
 
 @Catch(MulterError)
 export class MulterExceptionFilter implements ExceptionFilter {
-  catch(exception: MulterError, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const res = ctx.getResponse();
+	catch(exception: MulterError, host: ArgumentsHost) {
+		const ctx = host.switchToHttp();
+		const res = ctx.getResponse<Response>();
 
-    if (exception.code === 'LIMIT_FILE_SIZE') {
-      res.status(HttpStatus.PAYLOAD_TOO_LARGE).json({
-        statusCode: HttpStatus.PAYLOAD_TOO_LARGE,
-        message: 'File too large! Max size is 3MB.',
-        error: 'Payload Too Large'
-      });
-    } else {
-      res.status(HttpStatus.BAD_REQUEST).json({
-        statusCode: HttpStatus.BAD_REQUEST,
-        message: exception.message,
-        error: 'Bad Request'
-      });
-    }
-  }
+		// Set CORS headers manually since exception bypasses normal flow
+		const origin = ctx.getRequest().headers.origin;
+		if (origin) {
+			res.setHeader('Access-Control-Allow-Origin', origin);
+			res.setHeader('Access-Control-Allow-Credentials', 'true');
+		}
+
+		if (exception.code === 'LIMIT_FILE_SIZE') {
+			res.status(HttpStatus.PAYLOAD_TOO_LARGE).json({
+				statusCode: HttpStatus.PAYLOAD_TOO_LARGE,
+				message: 'File too large! Max size is 10MB.',
+				error: 'Payload Too Large',
+			});
+		} else {
+			res.status(HttpStatus.BAD_REQUEST).json({
+				statusCode: HttpStatus.BAD_REQUEST,
+				message: exception.message,
+				error: 'Bad Request',
+			});
+		}
+	}
 }
